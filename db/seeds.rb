400fa9371d80a3b7ef584e5fdec0e59949a9c5b7
Thomas Sohet <sohet.Thomas@gmail.com>
2017-10-31 19:53:23 +0100

    exo 2 -- DONE

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+puts "destroying the previous database"
+
+Article.destroy_all
+
+puts "starting to seed"
+puts "************************"
+
+15.times do
+  article = Article.create(title:Faker::HowIMetYourMother.character ,context: Faker::HowIMetYourMother.quote )
+  puts "created #{article.title}"
+end
+
+puts "************************"
+puts "ended to seed"
+
+
+# t.string "title"
+# t.text "context"
